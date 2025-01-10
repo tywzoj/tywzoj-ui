9554627d951f11b41e157bd7fed2fb8ad8f25a69
Antares <antares.mi@outlook.com>
2025-01-10 14:30:55 +0800

    fix

--- src/error/utils.ts
+++ src/error/utils.ts
@@ -1,6 +1,8 @@
 export function showErrorPage(error?: Error) {
     document.getElementById("error-box-stack")!.innerText = error?.stack || error?.message || "";
-
+    if (error) {
+        console.error(error);
+    }
     document.getElementById("error-box")!.style.display = "";
 }
 
