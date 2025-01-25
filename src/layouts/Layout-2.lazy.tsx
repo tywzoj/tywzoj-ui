e38ee31ff14f12f9751b3e7d366e2e07c72aa610
Antares <antares.mi@outlook.com>
2025-01-26 01:27:36 +0800

    update

--- src/layouts/Layout-2.lazy.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const LayoutLazy = React.lazy(() => import("./Layout-2"));
---
+++ src/layouts/Layout.lazy.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const LayoutLazy = React.lazy(() => import("./Layout"));
