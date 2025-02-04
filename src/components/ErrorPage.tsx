06bd05cbadf0db01cd7ddf6fcde99061d0ce4dec
Antares <antares.mi@outlook.com>
2025-02-04 16:58:41 +0800

    lint

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import type { IErrorLink, IStringCodeErrorLink } from "@/common/types/error-link";
 import { useLocalizedStrings } from "@/locales/hooks";
 
-import { AppError } from "../common/exceptions/app-error";
 import { ErrorBox } from "../common/components/ErrorBox";
+import { AppError } from "../common/exceptions/app-error";
 
 export interface IErrorPageProps {
     error: Error;
