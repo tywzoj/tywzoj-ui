c633792b7d540bcb123ca7715754bc6d487f791f
Antares <antares.mi@outlook.com>
2025-11-22 14:50:05 +0800

    use common sort

--- src/common/validators/user.ts
+++ src/common/validators/user.ts
@@ -1,11 +1,15 @@
 import { z } from "zod";
 
+import { CE_UserSortBy } from "@/server/modules/user.enums";
+
 import {
     EMAIL_MAX_LENGTH,
     NICKNAME_MAX_LENGTH,
     PASSWORD_MAX_LENGTH,
     PASSWORD_MIN_LENGTH,
 } from "../constants/data-length";
+
+export const Z_USER_SORTBY = z.enum([CE_UserSortBy.Id, CE_UserSortBy.AcceptedProblemCount, CE_UserSortBy.Rating]);
 
 export const Z_USERNAME = z.string().regex(/^[a-zA-Z0-9\-_.#$]{3,24}$/);
 export const Z_NICKNAME = z.string().max(NICKNAME_MAX_LENGTH);
